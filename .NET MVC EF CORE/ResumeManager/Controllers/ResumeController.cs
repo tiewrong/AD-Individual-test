69721ee98cf74224f8fde0cd156f923ae5d27e9a
tiewrong <129280403+tiewrong@users.noreply.github.com>
2025-05-09 23:14:51 +0800

    update

---
+++ .NET MVC EF CORE/ResumeManager/Controllers/ResumeController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResumeManager.Data;
+using ResumeManager.Models;
+
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ResumeManager.Controllers
+{
+    public class ResumeController : Controller
+    {
+        private readonly ResumeDbContext _context;
+
+        private readonly IWebHostEnvironment _webHost;
+        public ResumeController(ResumeDbContext context, IWebHostEnvironment webHost)
+        {
+            _context = context;
+            _webHost = webHost;
+        }
+        public IActionResult Index()
+        {
+            List<Applicant> applicants;
+            applicants = _context.Applicants.ToList();
+            return View(applicants);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            Applicant applicant = new Applicant();
+            applicant.Experiences.Add(new Experience() { ExperienceId = 1 });
+           // applicant.Experiences.Add(new Experience() { ExperienceId = 2 });
+           // applicant.Experiences.Add(new Experience() { ExperienceId = 3 });
+
+            return View(applicant);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Applicant applicant)
+        {
+            string uniqueFilename = GetUploadedFileName(applicant);
+            applicant.PhotoUrl = uniqueFilename;
+
+            _context.Add(applicant);
+            _context.SaveChanges();
+            return RedirectToAction("index");
+        }
+
+        private string GetUploadedFileName(Applicant applicant)
+        {
+            string uniqueFileName = null;
+
+            if (applicant.ProfilePhoto != null)
+            {
+                string uploadsFolder = Path.Combine(_webHost.WebRootPath, "images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + applicant.ProfilePhoto.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    applicant.ProfilePhoto.CopyTo(fileStream);
+                }
+            }
+
+            return uniqueFileName;
+        }
+
+    }
+}
